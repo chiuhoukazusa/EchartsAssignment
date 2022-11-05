d16cb4aefcb3c1dfcb1240576f177f54141546e3
chiuhoukazusa <chiuhoukazusa@foxmail.com>
2022-11-06 01:33:47 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,18 @@
+var chart = echarts.init(document.getElementById('main'));
+
+var option = {
+    grid3D: {},
+    xAxis3D: {},
+    yAxis3D: {},
+    zAxis3D: {},
+    series: [{
+        type: 'scatter3D',
+        symbolSize: 50,
+        data: [[-1, -1, -1], [0, 0, 0], [1, 1, 1]],
+        itemStyle: {
+            opacity: 1
+        }
+    }]
+};
+
+chart.setOption(option);
